543576123d4b92c8686a95792d8aa943348b4c67
Mike <MikeKondr99@gmail.com>
2025-11-11 02:25:53 +0300

    feat(core): внедрил TickerQ с тестовой джобой

--- src/ReData.DemoApplication/Extensions/WebApplicationExtension.cs
+++ src/ReData.DemoApplication/Extensions/WebApplicationExtension.cs
@@ -13,4 +13,5 @@ public static class WebApplicationExtension
             db.Database.Migrate();
         }
     }
+    
 }
--- src/ReData.DemoApplication/Program.cs
+++ src/ReData.DemoApplication/Program.cs
@@ -8,9 +8,14 @@ using ReData.DemoApplication.Database;
 using ReData.DemoApplication.Extensions;
 using ReData.DemoApplication.Services;
 using Scalar.AspNetCore;
+using TickerQ.Dashboard.DependencyInjection;
+using TickerQ.DependencyInjection;
+using TickerQ.EntityFrameworkCore.DbContextFactory;
+using TickerQ.EntityFrameworkCore.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
+var tickerQConnectinoString = builder.Configuration.GetConnectionString("TickerQ");
 
 if (builder.Environment.IsDevelopment())
 {
@@ -26,10 +31,34 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 });
 
 services.AddOutputCache();
+
 services.AddFastEndpoints();
 services.SwaggerDocument();
 
-services.AddDbContext<ApplicationDatabaseContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+services.AddTickerQ(options =>
+{
+    options.AddOperationalStore(efOptions =>
+    {
+        // Use built-in TickerQDbContext with connection string
+        efOptions.UseTickerQDbContext<TickerQDbContext>(optionsBuilder =>
+        {
+            // dotnet ef migrations add TickerQInitialCreate --context TickerQDbContext --project ./src/ReData.DemoApplication -o ./Jobs/Migrations
+            optionsBuilder.UseNpgsql(tickerQConnectinoString,
+                builder =>
+                {
+                    builder.EnableRetryOnFailure(3, TimeSpan.FromSeconds(5), ["40P01"]);
+                    builder.MigrationsAssembly("ReData.DemoApplication");
+                });
+        }, schema: "tickerq");
+    });
+    options.AddDashboard(dashboardOptions =>
+    {
+        dashboardOptions.SetBasePath("/api/tickerq");
+    });
+});
+
+services.AddDbContext<ApplicationDatabaseContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 services.AddSingleton<ConnectionService>();
 
@@ -38,9 +67,9 @@ var app = builder.Build();
 app.Use(async (context, next) =>
 {
     await next();
-    
+
     // If there's no available file and the request doesn't start with /api
-    if (context.Response.StatusCode == 404 && 
+    if (context.Response.StatusCode == 404 &&
         context.Request.Path.Value?.StartsWith("/api", StringComparison.Ordinal) != true)
     {
         context.Request.Path = "/index.html";
@@ -49,16 +78,16 @@ app.Use(async (context, next) =>
 });
 
 app.Migrate<ApplicationDatabaseContext>();
+app.Migrate<TickerQDbContext>();
+
 
 app.UseOutputCache();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 app.UseFastEndpoints();
 
-app.UseSwaggerGen(options =>
-{
-    options.Path = "/openapi/{documentName}.json";
-});
-app.MapScalarApiReference();
+app.UseSwaggerGen(options => { options.Path = "/openapi/{documentName}.json"; });
+app.UseTickerQ();
+app.MapScalarApiReference("api/docs");
 
 app.Run();
