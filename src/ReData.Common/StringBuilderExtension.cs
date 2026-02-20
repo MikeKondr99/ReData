a5233d3625c9055f8fa0db2c813b304c730ae218
Mike <MikeKondr99@gmail.com>
2026-02-20 14:29:02 +0300

    feat(core): add efficient AppendStringJoin overloads

--- src/ReData.Common/StringBuilderExtension.cs
+++ src/ReData.Common/StringBuilderExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace ReData.Common;
 
@@ -11,6 +12,44 @@ public static class StringBuilderExtension
         {
             length--;
         }
+
         sb.Length = length;
+    }
+
+    [OverloadResolutionPriority(1)]
+    public static void AppendStringJoin(this StringBuilder sb, string? separator, ReadOnlySpan<string?> values)
+    {
+        if (values.Length == 0)
+        {
+            return;
+        }
+
+        separator ??= string.Empty;
+        sb.Append(values[0]);
+        for (int i = 1; i < values.Length; i++)
+        {
+            sb.Append(separator);
+            sb.Append(values[i]);
+        }
+    }
+
+    [OverloadResolutionPriority(0)]
+    public static void AppendStringJoin(this StringBuilder sb, string? separator, IEnumerable<string?> values)
+    {
+        ArgumentNullException.ThrowIfNull(values);
+
+        using var enumerator = values.GetEnumerator();
+        if (!enumerator.MoveNext())
+        {
+            return;
+        }
+
+        separator ??= string.Empty;
+        sb.Append(enumerator.Current);
+        while (enumerator.MoveNext())
+        {
+            sb.Append(separator);
+            sb.Append(enumerator.Current);
+        }
     }
 }
